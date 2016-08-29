5ac33379826809a8dd97522ceb64cb7e7267692d
dwblair <donblair@gmail.com>
2016-08-29 00:01:56 -0400

    update

---
+++ simple_multi/moteino_lora_json_tx/moteino_lora_json_tx.ino
@@ -0,0 +1,148 @@
+// Feather9x_TX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (transmitter)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Feather9x_RX
+
+// Send voltage
+// https://learn.adafruit.com/adafruit-feather-m0-adalogger/power-management
+ 
+#include <SPI.h>
+#include <RH_RF95.h>
+#include "LowPower.h"
+#include <ArduinoJson.h>
+
+// Device ID
+#define DEVID 1
+
+/*
+ SLEEP_15MS,
+ SLEEP_30MS, 
+  SLEEP_60MS,
+  SLEEP_120MS,
+  SLEEP_250MS,
+  SLEEP_500MS,
+  SLEEP_1S,
+  SLEEP_2S,
+  SLEEP_4S,
+  SLEEP_8S,
+  SLEEP_FOREVER
+*/
+  
+
+// Change to 434.0 or other frequency, must match RX's freq!
+//#define RF95_FREQ 915.0
+#define RF95_FREQ 868.0
+ 
+// Singleton instance of the radio driver
+//RH_RF95 rf95(RFM95_CS, RFM95_INT);
+RH_RF95 rf95;
+
+// Blinky on receipt
+#define LED 9
+
+// Voltage PIN
+#define VBATPIN A7
+
+
+void setup() 
+{
+
+  
+  pinMode(LED, OUTPUT);
+  //pinMode(RFM95_RST, OUTPUT);
+  //digitalWrite(RFM95_RST, HIGH);
+ 
+//  while (!Serial);
+  Serial.begin(9600);
+  delay(100);
+ 
+  Serial.println(" LoRa TX Test!");
+ 
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+ 
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+ 
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+}
+ 
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+ 
+void loop()
+{ 
+  digitalWrite(LED, HIGH);
+  Serial.println("Sending to rf95_server");
+
+  float measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 1024; // convert to voltage
+  measuredvbat = 6;
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["id"] = DEVID;
+  root["millis"] = millis();
+  JsonObject& data = root.createNestedObject("data");
+  //data["vbat"] = double_with_n_digits(measuredvbat, 3);
+  data["vbat"] = analogRead(A0);
+
+  char buf[251];
+  root.printTo(buf, sizeof(buf));
+  buf[sizeof(buf)-1] = 0;
+  
+  Serial.print("Sending "); Serial.println(buf);
+  rf95.send((uint8_t *)buf, sizeof(buf));
+
+ 
+  Serial.println("Waiting for packet to complete..."); delay(10);
+  rf95.waitPacketSent();
+
+/*
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+ 
+  Serial.println("Waiting for reply..."); delay(10);
+  if (rf95.waitAvailableTimeout(1000))
+  { 
+    // Should be a reply message for us now   
+    if (rf95.recv(buf, &len))
+   {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);    
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    Serial.println("No reply, is there a listener around?");
+  }
+*/
+
+    digitalWrite(LED, LOW);
+
+  //delay(8000);
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);  
+  
+}
